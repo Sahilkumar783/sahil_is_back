ee1500fb2f7ccf19bd7cd945a50095034345a0f6
Sahilkumar783 <154525288+Sahilkumar783@users.noreply.github.com>
2025-07-04 17:29:49 +0530

    Delete subnet directory

--- subnet/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_subnet" "SNList" {
-    for_each = var.sn_name
-  name                 = each.value.snname
-  resource_group_name  = each.value.resource_group_name
-  virtual_network_name = each.value.virtual_network_name
-  address_prefixes     = each.value.address_prefixes
-}
